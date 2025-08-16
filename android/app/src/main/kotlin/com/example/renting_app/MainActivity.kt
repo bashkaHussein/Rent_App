1336d6158e69b5fed63274ccb708f552b657e758
bashkaHussein <bashir4989@diu.edu.so>
2025-08-16 14:52:18 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/renting_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.renting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class homescreen extends StatelessWidget {
+  const homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff151617),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xff151617),
+        foregroundColor: Colors.white,
+        leading: Icon(IconlyLight.search),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Badge(
+              alignment: Alignment(1, -1.5),
+              backgroundColor: Color(0xff724cf6),
+              child: Icon(IconlyLight.notification),
+            ),
+          ),
+        ],
+        title: ActionChip(label: Text("Bashir Hussein"),
+        shape: StadiumBorder(),
+        backgroundColor: Color(0xff272b30),
+        labelStyle: TextStyle(color: Colors.white),
+        side: BorderSide.none,
+        avatar: Icon(IconlyLight.location),
+        onPressed: (){},
+        ),
+      ),
+    );
+  }
+}
