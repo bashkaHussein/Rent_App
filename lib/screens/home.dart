13baa2e02fe584443061dc7cad1dc7db591e2a52
bashkaHussein <bashir4989@diu.edu.so>
2025-08-16 16:50:23 +0300

    adding some code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,17 +23,62 @@ class homescreen extends StatelessWidget {
             ),
           ),
         ],
-        title: ActionChip(label: Text("Muqdisho"),
-        shape: StadiumBorder(),
-        backgroundColor: Color(0xff272b30),
-        labelStyle: TextStyle(color: Colors.white),
-        side: BorderSide.none,
-        avatar: Icon(IconlyLight.location),
-        onPressed: (){},
+        title: ActionChip(
+          label: Text("Muqdisho"),
+          shape: StadiumBorder(),
+          backgroundColor: Color(0xff272b30),
+          labelStyle: TextStyle(color: Colors.white),
+          side: BorderSide.none,
+          avatar: Icon(IconlyLight.location),
+          onPressed: () {},
         ),
       ),
       body: ListView(
-        
+        children: [
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    'Find the Best Rent',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 98,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index){
+                      return Container(
+                        height: double.maxFinite,
+                        width: 90,
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(18)
+                        ),
+                        child: Column(
+                          children: [
+                            //category image
+
+
+                            //category name
+                          ],
+                        ),
+                      );
+
+                    },
+                    separatorBuilder: (context, index)=> SizedBox(width: 10,),
+                    itemCount: 8,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
